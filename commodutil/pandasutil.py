1b0fa13c3a2c066ff6a7d1f7cd5cc28b024ca77d
UI906879 <Asdfbpbp11>
2019-12-11 15:59:58 +0000

    adding circleci

--- commodutil/pandasutil.py
+++ commodutil/pandasutil.py
@@ -11,8 +11,10 @@ def mergets(left, right, leftl=None, rightl=None, how='left'):
     rename = {}
     if leftl is not None:
         rename[left.columns[0]] = leftl
+        rename[left.columns[0] + '_x'] = leftl
     if rightl is not None:
         rename[right.columns[0]] = rightl
+        rename[right.columns[0] + '_y'] = rightl
 
     res = res.rename(columns=rename)
 
