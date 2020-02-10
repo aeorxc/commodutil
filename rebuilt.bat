95a672e3e452a80b41dd2abf6f168360188d2c0c
UI906879 <Pear1234>
2020-02-10 13:27:51 +0000

    add command to install directly from git

--- rebuilt.bat
+++ rebuilt.bat
@@ -1,3 +1,4 @@
 pip uninstall -y commodutil
 python setup.py bdist_wheel
 pip install dist\commodutil-1.3.0-py2.py3-none-any.whl
+REM pip install git+https://github.com/aeorxc/commodutil#egg=commodutil
