4208ef55bbac92a224b096d3492438d668c596cc
UI906879 <Pear1234>
2020-03-31 19:55:47 +0100

    bump version

--- rebuilt.bat
+++ rebuilt.bat
@@ -1,4 +1,4 @@
 pip uninstall -y commodutil
 python setup.py bdist_wheel
-pip install dist\commodutil-1.3.0-py2.py3-none-any.whl
+pip install dist\commodutil-1.4.0-py2.py3-none-any.whl
 REM pip install git+https://github.com/aeorxc/commodutil#egg=commodutil
