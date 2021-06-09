f9fd57089fee32aa969f18477a1923df482b8051
aeorxc <aeorxc@gmail.com>
2021-06-09 09:14:23 +0100

    fix: gitlab remove licence check

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="commodutil",
-    version="1.0.36",
+    version="1.0.37",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity/oil analytics utils",
