ab63d90c1c9d7daf5a0ac940e9847377c1cfee04
Sanjeev Kumar <sanjeev.kumar2@rwe.com>
2024-01-19 11:01:07 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodutil",
-    version="1.2.21",
+    version="1.2.22",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity/oil analytics utils",
