0c3b364acfb4b9aec5f895d95fb3a8b11568bcf4
ui906879 <gibran.afzal@rwe.com>
2024-02-23 17:21:05 +0000

    bug fix for expiry contract removal

--- commodutil/forwards.py
+++ commodutil/forwards.py
@@ -693,7 +693,8 @@ def replace_last_month_with_nan(series):
     _, last_day = monthrange(last_month.year, last_month.month)
     last_valid_month_end = pd.to_datetime(f"{last_month.year}-{last_month.month}-{last_day}")
     # Replace series with NaN for the last valid month
-    series[last_month:last_valid_month_end] = np.nan
+    if last_month.year < dates.curyear:
+        series[last_month:last_valid_month_end] = np.nan
 
     return series
 
