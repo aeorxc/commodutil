d9ef3134d47a1ce0673780b87c50d0db65a4c9b0
ui906879 <gibran.afzal@rwe.com>
2025-04-16 13:37:43 +0100

    bring commodutils up to date

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodutil",
-    version="1.2.12",
+    version="2.0.0",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity/oil analytics utils",
