38677b35c5bf9eaee276978b0a3c30fcab82cc1b
ui906879 <gibran.afzal@rwe.com>
2025-06-27 18:15:57 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="commodutil",
-    version="2.3.7",
+    version="2.3.8",
     author="aeorxc",
     author_email="author@example.com",
     description="common commodity/oil analytics utils",
